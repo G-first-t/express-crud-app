dfeb4d7420e49cf7e2b61121e199d477c3f85b2b
gregory <gregorykimbira7@gmail.com>
2025-03-06 15:40:54 +0300

    created index.js which is the root of our application , it shows the view engine [ ejs] , the connection with mongoose

---
+++ express-crud/index.js
@@ -0,0 +1,27 @@
+const express= require('express');
+const mongoose= require('mongoose');
+
+const app = express();
+
+
+const router=require('./routes/routes');
+
+
+//set view
+app.set('view engine', 'ejs');
+
+
+//database connection
+mongoose.connect('https://localhost:27017/UsersDB')
+    .then(()=>console.log('connected successfully'))
+    .catch((err)=>console.error('Failed due to::',err));
+
+
+app.use(router);
+
+app.listen(3000, (err)=>{
+    if(err){
+        console.error('Failed due to ::',err);
+    }
+    console.log('application run on port 3000');
+})
